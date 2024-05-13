55b4b8fb6add6f5d41d85a229faa63f1c440645c
Sergio Avila <sergioavila2720@gmail.com>
2024-05-12 19:06:05 -0700

    Initial commit

---
+++ order_routes.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+order_router=APIRouter(
+    prefix='/order',
+    tags=['orders']
+)
+
+@order_router.get('/')
+async def hello():
+    return {'message':'Hello from order'}
